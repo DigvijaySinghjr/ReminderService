5a015dbc84fddc53d3e86553a7f919faa85511b4
DigvijaySinghjr <neymar9838jr@gmail.com>
2025-07-16 11:17:58 +0530

    Added notification ticket model

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
 
-const { senderBasicEmail } = require('./services/email-service');
+//const { senderBasicEmail } = require('./services/email-service');
+
+const cron = require('node-cron'); 
 
 const app = setupAndStartServer = () =>{
     const app = express();
@@ -13,12 +15,17 @@ const app = setupAndStartServer = () =>{
     app.listen(PORT, ()=>{
         console.log(`server started at ${PORT}`);
 
-        senderBasicEmail(
-            'randomSender@gmail.com',
-             'neymar9838jr@gmail.com',
-            'this is a testing email',
-            'Hope you like the support'
-        );
+        // senderBasicEmail(
+        //     'randomSender@gmail.com',
+        //      'neymar9838jr@gmail.com',
+        //     'this is a testing email',
+        //     'Hope you like the support'
+        // );
+    
+
+cron.schedule('*/10 * * * * *', () => {
+  console.log('running a task at every 10 secoands');
+});
     });
 }
 
